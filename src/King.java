6b901040ff81447b85a2b4adada23ef24a658d4c
AlexeyGochachilov <agochachilov@gmail.com>
2024-10-31 18:05:33 +0300

    Update King.java

--- src/King.java
+++ src/King.java
@@ -27,140 +27,132 @@ public class King extends ChessPiece {
 
     public boolean isUnderAttack(ChessBoard chessBoard, int line, int column) {
         boolean b = false;
-        int raznost = column > line ? column : line;
-        if (true) {
+        int raznost1 = column > line ? column : line;
+        int raznost2 = line >= 7 - column ? 7 - line : column;
+        int raznost3 = column >= 7 - line ? 7 - column : line;
 
+        OUTER:
+        for (int a = 0; a < 1; a++) {
+
+            INNER1:
             for (int i = 1; i <= 7 - column; i++) {
                 if (chessBoard.board[line][column + i] != null) {
                     if (!chessBoard.board[line][column + i].getColor().equals(chessBoard.nowPlayer) &&
                             (chessBoard.board[line][column + i].getSymbol().equals("Q") ||
                                     chessBoard.board[line][column + i].getSymbol().equals("R"))) {
                         b = true;
-                        break;
-                    } else {
-                        b = false;
-                        break;
                     }
+                    break OUTER;
                 }
             }
 
+            INNER2:
             for (int i = 1; i <= column; i++) {
                 if (chessBoard.board[line][column - i] != null) {
                     if (!chessBoard.board[line][column - i].getColor().equals(chessBoard.nowPlayer) &&
                             (chessBoard.board[line][column - i].getSymbol().equals("Q") ||
                                     chessBoard.board[line][column - i].getSymbol().equals("R"))) {
                         b = true;
-                        break;
-                    } else {
-                        b = false;
-                        break;
                     }
+                    break OUTER;
                 }
             }
 
+            INNER3:
             for (int i = 1; i <= 7 - line; i++) {
                 if (chessBoard.board[line + i][column] != null) {
                     if (!chessBoard.board[line + i][column].getColor().equals(chessBoard.nowPlayer) &&
                             (chessBoard.board[line + i][column].getSymbol().equals("Q") ||
                                     chessBoard.board[line + i][column].getSymbol().equals("R"))) {
                         b = true;
-                        break;
-                    } else {
-                        b = false;
-                        break;
                     }
+                    break OUTER;
                 }
             }
 
+            INNER4:
             for (int i = 1; i <= line; i++) {
                 if (chessBoard.board[line - i][column] != null) {
                     if (!chessBoard.board[line - i][column].getColor().equals(chessBoard.nowPlayer) &&
                             (chessBoard.board[line - i][column].getSymbol().equals("Q") ||
                                     chessBoard.board[line - i][column].getSymbol().equals("R"))) {
                         b = true;
-                        break;
-                    } else {
-                        b = false;
-                        break;
                     }
+                    break OUTER;
                 }
             }
 
-            for (int i = 1; i <= 7 - raznost; i++) {
+            INNER5:
+            for (int i = 1; i <= 7 - raznost1; i++) {
                 if (chessBoard.board[line + i][column + i] != null) {
                     if (!chessBoard.board[line + i][column + i].getColor().equals(chessBoard.nowPlayer) &&
                             (chessBoard.board[line + i][column + i].getSymbol().equals("Q") ||
                                     chessBoard.board[line + i][column + i].getSymbol().equals("B"))) {
                         b = true;
-                        break;
-                    } else {
-                        b = false;
-                        break;
                     }
+                    break OUTER;
                 }
             }
 
-            for (int i = 1; i <= 7 - raznost; i++) {
+            INNER6:
+            for (int i = 1; i <= raznost2; i++) {
                 if (chessBoard.board[line + i][column - i] != null) {
                     if (!chessBoard.board[line + i][column - i].getColor().equals(chessBoard.nowPlayer) &&
                             (chessBoard.board[line + i][column - i].getSymbol().equals("Q") ||
                                     chessBoard.board[line + i][column - i].getSymbol().equals("B"))) {
                         b = true;
-                        break;
-                    } else {
-                        b = false;
-                        break;
                     }
+                    break OUTER;
                 }
             }
 
-            for (int i = 1; i <= 7 - raznost; i++) {
+            INNER7:
+            for (int i = 1; i <= raznost3; i++) {
                 if (chessBoard.board[line - i][column + i] != null) {
                     if (!chessBoard.board[line - i][column + i].getColor().equals(chessBoard.nowPlayer) &&
                             (chessBoard.board[line - i][column + i].getSymbol().equals("Q") ||
                                     chessBoard.board[line - i][column + i].getSymbol().equals("B"))) {
                         b = true;
-                        break;
-                    } else {
-                        b = false;
-                        break;
                     }
+                    break OUTER;
                 }
             }
 
-            for (int i = 1; i <= raznost; i++) {
+            INNER8:
+            for (int i = 1; i <= raznost1; i++) {
                 if (chessBoard.board[line - i][column - i] != null) {
                     if (!chessBoard.board[line - i][column - i].getColor().equals(chessBoard.nowPlayer) &&
                             (chessBoard.board[line - i][column - i].getSymbol().equals("Q") ||
                                     chessBoard.board[line - i][column - i].getSymbol().equals("B"))) {
                         b = true;
-                        break;
-                    } else {
-                        b = false;
-                        break;
                     }
+                    break OUTER;
                 }
             }
 
             if (line - 2 >= 0 && line + 2 <= 7 && column - 1 >= 0 && column + 1 <= 7) {
+                MIDEL1:
                 for (int i = -2; i < 3; i += 4) {
+                    INNER1:
                     for (int j = -1; j < 2; j += 2) {
                         if (!chessBoard.board[line + i][column + j].getColor().equals(chessBoard.nowPlayer) &&
                                 chessBoard.board[line + i][column + j].getSymbol().equals("H")) {
                             b = true;
-                            break;
+                            break MIDEL1;
                         }
                     }
                 }
             }
 
             if (line - 1 >= 0 && line + 1 <= 7 && column - 2 >= 0 && column + 2 <= 7) {
+                MIDEL2:
                 for (int i = -2; i < 3; i += 4) {
+                    INNER2:
                     for (int j = -1; j < 2; j += 2) {
                         if (!chessBoard.board[line + j][column + i].getColor().equals(chessBoard.nowPlayer) &&
                                 chessBoard.board[line + j][column + i].getSymbol().equals("H")) {
                             b = true;
-                            break;
+                            break MIDEL2;
                         }
                     }
                 }
@@ -172,10 +164,8 @@ public class King extends ChessPiece {
     public boolean moveKing(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
         boolean b = false;
         if (chessBoard.board[toLine][toColumn] == null || !chessBoard.board[toLine][toColumn].getColor().equals(chessBoard.nowPlayer)) {
-            if (!isUnderAttack(chessBoard, toLine, toColumn)) {
-                if ((toLine == (line + 1)) || toLine == (line - 1) || toColumn == (column + 1) || toColumn == (column - 1)) {
-                    b = true;
-                }
+            if ((toLine == (line + 1)) || toLine == (line - 1) || toColumn == (column + 1) || toColumn == (column - 1)) {
+                b = !isUnderAttack(chessBoard, toLine, toColumn);
             }
         }
         return b;
